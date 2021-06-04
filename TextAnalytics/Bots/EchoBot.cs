095671348618c477d0b574f2f291d985dfb5e267
twdeveloper <twdeveloper@outlook.com>
2021-06-04 22:21:37 +0800

    v1

---
+++ TextAnalytics/Bots/EchoBot.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.13.2
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+namespace TextAnalytics.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var ret = TextAnalytics.DetectLanguage(turnContext.Activity.Text);
+            var replyText = $"Echo: {turnContext.Activity.Text} ";
+            replyText += $" {System.Environment.NewLine} 語系是: {ret.DetectedLanguages[0].Name}";
+            replyText += $" {System.Environment.NewLine} Get Sentiment Score: { TextAnalytics.GetSentiment(turnContext.Activity.Text).Score}";
+
+            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Hello and welcome!";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+    }
+}
---
+++ TextAnalytics/Bots/TextAnalytics.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TextAnalytics.Bots
+{
+    public class TextAnalytics
+    {
+        public static LanguageResult DetectLanguage(string Text)
+        {
+            var cogsKey = "____________14a91bb83292_________";
+            var cogsBaseUrl = "https://___________.cognitiveservices.azure.com/";
+            var credentials = new ApiKeyServiceClientCredentials(cogsKey);
+            TextAnalyticsClient client = new TextAnalyticsClient(credentials)
+            {
+                Endpoint = cogsBaseUrl
+            };
+
+            var result = client.DetectLanguage(Text, "us");
+            return result;
+        }
+
+        public static SentimentResult GetSentiment(string Text)
+        {
+            var cogsKey = "____________14a91bb83292_________";
+            var cogsBaseUrl = "https://__________.cognitiveservices.azure.com/";
+            var credentials = new ApiKeyServiceClientCredentials(cogsKey);
+            TextAnalyticsClient client = new TextAnalyticsClient(credentials)
+            {
+                Endpoint = cogsBaseUrl
+            };
+
+            var result = client.Sentiment(Text, "en");
+            return result;
+        }
+    }
+}
